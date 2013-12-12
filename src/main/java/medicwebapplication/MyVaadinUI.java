9df685374fd9d60930e6d7c5334b3d784d0ef60c
R <r@oo.pl>
2013-12-12 21:56:32 +0100

    add basic vaadin theme

--- src/main/java/medicwebapplication/MyVaadinUI.java
+++ src/main/java/medicwebapplication/MyVaadinUI.java
@@ -2,9 +2,6 @@ package medicwebapplication;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import consumingrestobjects.Greeting;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -14,13 +11,6 @@ import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import consumingrestobjects.Jednostka;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 @Theme("mytheme")
 @SuppressWarnings("serial")
@@ -28,28 +18,27 @@ public class MyVaadinUI extends UI {
 
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class)
-    //, widgetset = "medicwebapplication.AppWidgetSet" problem w configuration
     public static class Servlet extends VaadinServlet {
     }
 
     @Override
     protected void init(VaadinRequest request) {
-        ArrayList<Jednostka> parsingResponse = null;
-        String nameUnitT = "";
-
-        LinkedHashMap getVariables = new LinkedHashMap();
-        getVariables.put("phrase", "iniczny");
-
-        RestTemplate restTemplate = new RestTemplate();
-        ObjectMapper mapper = new ObjectMapper();
-//        ResponseEntity<ArrayList> pojUnit = restTemplate.getForEntity("http://localhost:8080/search?phrase={phrase}", ArrayList.class, getVariables);
-        String unitsString = restTemplate.getForObject("http://localhost:8080/search?phrase={phrase}", String.class, getVariables);
-        try {
-           parsingResponse = mapper.readValue(unitsString, new TypeReference<ArrayList<Jednostka>>() {});
-        } catch (IOException e) {
-            System.out.print("Parsing array error");
-            e.printStackTrace();
-        }
+//        ArrayList<Jednostka> parsingResponse = null;
+//        String nameUnitT = "";
+//
+//        LinkedHashMap getVariables = new LinkedHashMap();
+//        getVariables.put("phrase", "iniczny");
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        ObjectMapper mapper = new ObjectMapper();
+//        String unitsString = restTemplate.getForObject("http://localhost:8080/search?phrase={phrase}", String.class, getVariables);
+//        try {
+//            parsingResponse = mapper.readValue(unitsString, new TypeReference<ArrayList<Jednostka>>() {
+//            });
+//        } catch (IOException e) {
+//            System.out.print("Parsing array error");
+//            e.printStackTrace();
+//        }
 
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
